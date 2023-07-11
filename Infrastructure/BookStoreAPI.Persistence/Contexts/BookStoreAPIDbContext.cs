778aa677d95b09392954a874c039cc977c7d04a1
huseyin corakli <huseyincorakli@icloud.com>
2023-07-11 10:11:34 +0300

    Add project files.

---
+++ Infrastructure/BookStoreAPI.Persistence/Contexts/BookStoreAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Persistence.Contexts
+{
+    public class BookStoreAPIDbContext : DbContext
+    {
+        public BookStoreAPIDbContext(DbContextOptions options) : base(options)
+        {}
+        DbSet<Product>Products { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Infrastructure/BookStoreAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using BookStoreAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<BookStoreAPIDbContext>(options=>options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
